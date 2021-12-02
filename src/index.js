92c6cd4daef45e6cf17a71a96d9a42016bc34491
Manuel <tofuah@gmail.com>
2021-12-02 18:41:58 +0000

    imported bootstrap and store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import store from "./store/Store"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <Provider store={store}>
